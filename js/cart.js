a2c6af581d656131714483beb6701bd69b82782a
Martin Paradiso <martinparadiso024@gmail.com>
2020-12-20 00:22:18 -0300

    cart

--- js/cart.js
+++ js/cart.js
@@ -1,58 +1,77 @@
 // Carrito
 var cart = [];
 
-// Funcion para boton agregar al Carrito
+// agregar productos al array
 function agregarAlCarrito(producto, cantidad){
     let productoParaElCarrito = producto;
+    let intCantidad = parseInt(cantidad);
     
     if (Array.isArray(cart) && cart.length){
         // array exists and is not empty
         cart.forEach(element => {
             if (element.name == productoParaElCarrito.name){
-                element.quantity = (element.quantity + cantidad);
+                element.quantity = (element.quantity + intCantidad);
                 console.log('Cantidad de producto actualizada');
+                updatePageCart();
             }
             else{
-                productoParaElCarrito.quantity = cantidad;
+                productoParaElCarrito.quantity = intCantidad;
                 cart.push(productoParaElCarrito);
                 console.log('Producto agregado al carrito');
+                addItemToPageCart();
             }
         });
     }
     else{
-        productoParaElCarrito.quantity = cantidad;
+        productoParaElCarrito.quantity = intCantidad;
         cart.push(productoParaElCarrito);
         console.log('Producto agregado al carrito');
+        addItemToPageCart();
     };
-    updatePageCart();
+
 };
 
 // Actualizar carrito cuando se agregan productos
-function updatePageCart(producto){
+function addItemToPageCart(){
 
-    $('shoppingCart').empty();
+    // Recorro Carrito
     cart.forEach(product => {
-        // TERMINAR ESTOOOOO
+
+        let cartItemContainer = document.createElement('li');
+        cartItemContainer.classList = 'cartItemContainer is-flex justify-content-center';
+
+        let cartItemImg = document.createElement('img');
+        cartItemImg.src = product.img;
+        cartItemImg.style.height = '50px';
+        cartItemImg.style.width = '50px';
+
+        let cartItem = document.createElement('div');
+
+        let cartItemName = document.createElement('div');
+        cartItemName.innerHTML = product.name;
+        cartItemName.classList = 'has-background-success has-text-light';
+
+        let cartItemPrice = document.createElement('div');
+        cartItemPrice.innerHTML = ('$' + product.price);
+
+        let cartItemQ = document.createElement('div');
+        cartItemQ.innerHTML = (product.quantity + ' unidades');
+        cartItemQ.classList = 'has-text-light has-background-dark';
+
+        
+        cartItem.appendChild(cartItemPrice);
+        cartItem.appendChild(cartItemQ);
+        cartItemContainer.appendChild(cartItemName);
+        cartItemContainer.appendChild(cartItemImg);
+        cartItemContainer.appendChild(cartItem);
+        $('#shoppingCart').append(cartItemContainer);
     });
 };
 
 
-// Ver carrito
-let cartContainer = document.getElementById('cartContainer');
-let cartButton = document.getElementById('cartButton');
 
-$('#cartButton').click( () => {
-    cartContainer.style.display = 'flex';
-});
 
-$('#hideCartButton').click( () => {
-    cartContainer.style.display = 'none';
-});
-
-// Carrito de compras
-
-    
-
+// Botones de carrito de compras
 
     // boton vaciar con Jquery
     $('#buttonVaciarCarrito').click(function(){
@@ -64,4 +83,17 @@ $('#hideCartButton').click( () => {
     $('#buttonComprar').click(function(){
         alert('Compra aceptada! redirigiendo a la pasarela de pago');
         localStorage.clear();
+    });
+
+    // botones Ver y esconder Carrito  
+    let cartContainer = document.getElementById('cartContainer');
+    let cartButton = document.getElementById('cartButton');
+
+
+    $('#cartButton').click( () => {
+    cartContainer.style.display = 'flex';
+    });
+
+    $('#hideCartButton').click( () => {
+    cartContainer.style.display = 'none';
     });
